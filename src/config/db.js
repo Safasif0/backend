47bbf24ca8625278a15971a0287ed98dceda2cf4
seif <seif.mohamed348@gmail.com>
2025-12-23 01:09:59 +0200

    db

--- src/config/db.js
+++ src/config/db.js
@@ -7,5 +7,5 @@ const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected on" + MONGO_URI))
+  .then(() => console.log("✅ MongoDB ba2t connected "))
   .catch((err) => console.error("❌ Mongo Error:", err));
